d9c5d9b43ec0862f4ebf84b4d9a9c2cb3390b420
NathanNicholson <thenathannicholson@gmail.com>
2022-09-20 09:39:37 -0700

    Refactor code and create full MVC structure

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,37 +1,54 @@
+import { NextFunction, Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import { User } from "../types";
+import { handleGetUsers, handleCreateUser } from "../models/users";
 
 const prisma = new PrismaClient();
 
-export async function getUsers() {
-  const allUsers = await prisma.users.findMany({
-    select: {
-      id: true,
-      walletId: true,
-      role: true,
-      email: true,
-      name: true,
-      discord: true,
-    },
-  });
-  return allUsers;
+// Get All of the Users
+
+export async function getUsers(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const data = await handleGetUsers();
+    console.log(data);
+    res.status(201).send(data);
+    await prisma.$disconnect();
+  } catch (e) {
+    console.error(e);
+    res.status(400).send(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
 }
 
-export async function createUser({
-  email,
-  name,
-  role,
-  walletId,
-  discord,
-}: User) {
-  const user = await prisma.users.create({
-    data: {
+// Create A New User
+
+export async function createUser(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { email, name, role, walletId, discord }: User = req.body;
+
+  try {
+    const data = await handleCreateUser({
       email,
       name,
       role,
       walletId,
       discord,
-    },
-  });
-  return user;
+    });
+    console.log(data);
+    res.status(201).send(data);
+    await prisma.$disconnect();
+  } catch (e) {
+    console.error(e);
+    res.status(400).send(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
 }
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,56 +1,14 @@
 import express from "express";
-import { PrismaClient } from "@prisma/client";
 import { getUsers, createUser } from "../controllers/users";
-import { User } from "../types";
 
 const router = express.Router();
 
-const prisma = new PrismaClient();
-
 // Get All of the Users
 
-router.get("/", async (req, res, next) => {
-  try {
-    const data = await getUsers();
-
-    console.log(data);
-
-    res.status(201).send(data);
-
-    await prisma.$disconnect();
-  } catch (e) {
-    console.error(e);
-
-    res.status(400).send(e);
-
-    await prisma.$disconnect();
-
-    process.exit(1);
-  }
-});
+router.get("/", getUsers);
 
 // Create A New User
 
-router.post("/", async (req, res, next) => {
-  const { email, name, role, walletId, discord }: User = req.body;
-
-  try {
-    const data = await createUser({ email, name, role, walletId, discord });
-
-    console.log(data);
-
-    res.status(201).send(data);
-
-    await prisma.$disconnect();
-  } catch (e) {
-    console.error(e);
-
-    res.status(400).send(e);
-
-    await prisma.$disconnect();
-
-    process.exit(1);
-  }
-});
+router.post("/", createUser);
 
 export default router;
