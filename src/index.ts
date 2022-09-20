7606f008e3be3b6497ae8d67ee969234e7d78f2d
NathanNicholson <thenathannicholson@gmail.com>
2022-09-19 17:25:34 -0700

    Create routes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { PrismaClient } from "@prisma/client";
+import usersRoute from "./routes/users";
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -8,70 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
-const prisma = new PrismaClient();
-
-app.get("/users", (req, res, next) => {
-  let data: any;
-
-  async function getUsers() {
-    const allUsers = await prisma.users.findMany({
-      select: {
-        id: true,
-        walletId: true,
-        role: true,
-        email: true,
-        name: true,
-        discord: true,
-      },
-    });
-    data = allUsers;
-  }
-
-  getUsers()
-    .then(async () => {
-      console.log(data);
-      res.status(201).send(data);
-      await prisma.$disconnect();
-    })
-    .catch(async (e) => {
-      console.error(e);
-      res.status(400).send(e);
-      await prisma.$disconnect();
-      process.exit(1);
-    });
-});
-
-app.post("/users", (req, res, next) => {
-  const { email, name, role, walletId, discord } = req.body;
-
-  let data: any;
-
-  async function createUser() {
-    const user = await prisma.users.create({
-      data: {
-        email,
-        name,
-        role,
-        walletId,
-        discord,
-      },
-    });
-    data = user;
-  }
-
-  createUser()
-    .then(async () => {
-      console.log(data);
-      res.status(201).send(data);
-      await prisma.$disconnect();
-    })
-    .catch(async (e) => {
-      console.log(e);
-      res.status(400).send(e);
-      await prisma.$disconnect();
-      process.exit(1);
-    });
-});
+app.use("/users", usersRoute);
 
 app.listen(port, () => {
   console.log(`Server running on PORT: ${port}`);
